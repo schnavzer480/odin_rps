263dacbdd8c59eb8fca264bd894137831e1291fb
Christoffer Jonsson <schnavzer@protonmail.com>
2024-07-29 18:48:55 +0200

    first try with js

---
+++ script.js
@@ -0,0 +1,26 @@
+/*let getComputerChoice = Math.floor(Math.random() * 3) + 1;
+console.log(getComputerChoice);*/
+
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3) +1;
+}
+
+/*if (getComputerChoice() == 1) {
+    console.log("Rock");
+} if (getComputerChoice() == 2) {
+    console.log("Paper")
+}   if (getComputerChoice() == 3) {
+    console.log("Scissors")
+}*/
+
+let choice = getComputerChoice();
+
+if (choice == 1) {
+    console.log("Rock");
+}
+else if (choice == 2) {
+    console.log("Paper");
+}
+else if (choice == 3) {
+    console.log("Scissors");
+}
